bcbcd5fa34b9c321d5b647a173b06ba3e90c1781
SiYangming <yangming-6863@qq.com>
2021-02-05 21:09:54 +0800

    v2.28.0

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,12 @@
 # need to do something to ensure destruction of JVM
 # when package is unloaded
 
-.First.lib <- function(libname,pkgname)
+.onLoad <- function(libname,pkgname)
  {
+ msg <- paste("\n*** Deprecation warning ***:\nThe package '%s' is",
+   "deprecated and will not be supported\nafter Bioconductor",
+    "release 2.9.\n\n")
+ packageStartupMessage(sprintf(msg, pkgname))
  library.dynam("RMAGEML",pkgname,libname)
  require(methods)
  .Call("rmageml_init", PACKAGE="RMAGEML")	 
